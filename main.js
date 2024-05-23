6db5b9fb2e8881e32909e022711971e5d86b245a
hpham474 <hpham474@gmail.com>
2024-05-23 00:05:11 -0500

    create variables to hold operators and numbers and function to operate on numbers

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ function divide(a, b) {
     return a - b;
 }
 
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+        default:
+            break;
+    }
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
